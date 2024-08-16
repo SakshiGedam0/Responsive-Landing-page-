2262de02f2cfcb47d8b103f3a812fa38cfee2c9d
Sakshi Gedam <gedams569@gmail.com>
2024-08-16 14:58:22 +0530

    Add files via upload

---
+++ responsive landing page/script.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', () => {
+  const navbar = document.querySelector('.navbar');
+  if (window.scrollY > 50) {
+    navbar.style.backgroundColor = '#222';
+  } else {
+    navbar.style.backgroundColor = '#333';
+  }
+});
+
+  
